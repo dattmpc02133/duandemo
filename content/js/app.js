5f3167a25d29ab0241e0fa607e41c6a805086143
nhanbwebsite <nhanbwebsite@gmail.com>
2021-11-11 15:44:16 +0700

    new text js and css

---
+++ content/js/app.js
@@ -0,0 +1,38 @@
+const app = {
+    hien_thi_account(){
+        var account_click = document.getElementById('account_dangnhap');
+        var account_on = document.getElementById('header-dropdown_content');
+        account_click.onclick = function(){
+            var cart_on = document.getElementById("header_cart-list");
+            account_on.classList.toggle('active');
+            cart_on.classList.remove('active');
+        }
+    },
+    hien_thi_gio_hang(){
+        var cart_click = document.getElementById('cart');
+        var cart_on = document.getElementById("header_cart-list");
+        cart_click.onclick = function(){
+            var account_on = document.getElementById('header-dropdown_content');
+            cart_on.classList.toggle('active');
+            account_on.classList.remove('active')
+        }
+    },
+    hidden_something(){
+        var hidden__something = document.getElementById('hidden__something');
+        hidden__something.onclick = function(){
+            var account_on = document.getElementById('header-dropdown_content');
+            var cart_on = document.getElementById("header_cart-list");
+            account_on.classList.remove('active');
+            cart_on.classList.remove('active');
+            
+        }
+
+    }
+    ,
+    start(){
+        this.hien_thi_account();
+        this.hien_thi_gio_hang()
+        this.hidden_something()
+    }
+}
+app.start();
