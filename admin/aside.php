aae8c3b532d3436a999816e9a2f10031ecb44054
nhanbwebsite <nhanbwebsite@gmail.com>
2021-11-16 09:59:13 +0700

    admin

---
+++ admin/aside.php
@@ -0,0 +1,52 @@
+<i class="fas fa-align-justify toggle-icon" id="toggle-icon"></i>
+                        <div class="account_manage-product">
+                            <div class="div_hinh">
+                            <img src="<?= $CONTENT_URL ?>/images/sofa-giuong-keo.jpg" alt="Hình Đại Diện" class="account_manage-img">
+                            </div>
+                            <ul class="account_manage-lish" id= "account_manage-lish">                 
+                                    <div class="account_manage-ten" id="account_manage-ten">
+                                        <h4 style="text-align: center;">ADMIN</h4>
+                                        <i class="account_manageincon fas fa-caret-down"></i>
+                                    </div>
+                               <div class="account_manage-items_block" id= "account_manage-items_block">
+                                    <li class="account_manage-items">
+                                        <a href="" class="account_manage-links"><i class="fas fa-exchange-alt"></i> Đổi Mật Khẩu</a>
+                                    </li>
+                                    <li class="account_manage-items">
+                                        <a href="" class="account_manage-links"><i class="far fa-edit"></i> Cập Nhật Tài Khoản</a>
+                                    </li>
+                                    <li class="account_manage-items">
+                                        <a href="" class="account_manage-links"><i class="fas fa-cogs"></i> Quản Trị Website</a>
+                                    </li>
+                                    <li class="account_manage-items">
+                                        <a href="" class="account_manage-links"><i class="fas fa-sign-out-alt"></i> Đăng Xuất</a>
+                                    </li>
+                               </div>
+                            </ul>
+                            <!---->
+                            <div class="sidebar_menu-product">
+                                <ul class="sidebar_menu">
+                                    <li class="sidebar_menu-lish">
+                                        <a href="" class="sidebar_menu-links"><i class="fas fa-home"></i> Trang Chủ</a>
+                                    </li>
+                                    <li class="sidebar_menu-lish">
+                                        <a href="" class="sidebar_menu-links"><i class="far fa-chart-bar"></i> Loại Hàng</a>
+                                    </li>
+                                    <li class="sidebar_menu-lish">
+                                        <a href="" class="sidebar_menu-links"><i class="fas fa-box-open"></i> Hàng Hóa</a>
+                                    </li>
+                                    <li class="sidebar_menu-lish">
+                                        <a href="" class="sidebar_menu-links"><i class="far fa-user-circle"></i> Khách Hàng</a>
+                                    </li>
+                                    <li class="sidebar_menu-lish">
+                                        <a href="" class="sidebar_menu-links"><i class="far fa-comments"></i> Bình Luận</a>
+                                    </li>
+                                    <li class="sidebar_menu-lish">
+                                        <a href="" class="sidebar_menu-links"><i class="fas fa-chart-pie"></i> Thống Kê</a>
+                                    </li>
+                                    <li class="sidebar_menu-lish">
+                                        <a href="" class="sidebar_menu-links"><i class="far fa-calendar"></i> Quản Lý Đơn Hàng</a>
+                                    </li>
+                                </ul>
+                            </div>
+</div>
