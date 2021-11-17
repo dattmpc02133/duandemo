db84f8162a98674dc8d56871be60d9b413ac0afe
nhanbwebsite <nhanbwebsite@gmail.com>
2021-11-17 20:06:10 +0700

    edit san pham

--- admin/san-pham/ajax_action.php
+++ admin/san-pham/ajax_action.php
@@ -15,17 +15,28 @@ if(isset($_POST['ten_sp'])){
     $dac_biet = $_POST['dac_biet'];
     $luot_xem = $_POST['luot_xem'];
     $ma_loai = $_POST['ma_loai'];
-    $hinh2 = save_file($hinh, $path);
-    san_pham_insert(
-        $ten_sp, 
-        $don_gia, 
-        $giam_gia, 
-        $hinh2, 
-        $so_luong, 
-        $trang_thai, 
-        $dac_biet, 
-        $luot_xem, 
-        $ma_loai);
+    // ràng buộc ảnh
+    $extension = explode(".", $_FILES['hinh']['name']);
+    $file_extension = end($extension);
+    $agree_type_img = array('jpg',"png","jpeg");
+    if(in_array($file_extension,$agree_type_img)){
+        $hinh2 = save_file($hinh, $path);
+        san_pham_insert(
+            $ten_sp, 
+            $don_gia, 
+            $giam_gia, 
+            $hinh2, 
+            $so_luong, 
+            $trang_thai, 
+            $dac_biet, 
+            $luot_xem, 
+            $ma_loai
+        );
+        echo "Thêm sản phẩm thành công";
+    } else{
+        echo "Ảnh không hợp lệ, vui lòng chọn ảnh có định dạng jpg, png, jpeg ";
+    }
+   
 } 
 
 ?>
