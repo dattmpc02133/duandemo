c80bd7037a3cbdb7aefcb0a2dfefad237894ae56
duynhpc02604 <duynhpc02604@fpt.edu.vn>
2021-11-23 14:45:58 +0700

    sua loi update khach hang

--- admin/khach-hang/update.php
+++ admin/khach-hang/update.php
@@ -19,7 +19,7 @@ if (isset($_POST['btn_update1'])) {
             }
             $email=$_POST['email'];
             $vai_tro=$_POST['vai_tro'];
-            khach_hang_update($mat_khau, $ho_ten, $dia_chi, $kich_hoat, $hinh, $email, $vai_tro, $ma_kh);
+            khach_hang_update($mat_khau, $ho_ten, $dia_chi, $kich_hoat, $tenhinh, $email, $vai_tro, $ma_kh);
             unset($_SESSION['ma_kh']);
            echo "<script> location.href = 'index.php'; </script>";
         } else {
