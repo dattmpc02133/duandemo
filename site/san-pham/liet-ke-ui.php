080ef3152887d1bdc3fab6709372e3ef0042be0c
duynhpc02604 <duynhpc02604@fpt.edu.vn>
2021-11-26 10:27:25 +0700

    sua loi hien thi gia tren trang san pham va trang chinh

--- site/san-pham/liet-ke-ui.php
+++ site/san-pham/liet-ke-ui.php
@@ -56,7 +56,11 @@
                                     <p> <?= $ten_sp ?> </span></p>
                                 </div>
                                 <div class="product-price">
-                                    <p><?= number_format($don_gia - ($giam_gia * $don_gia / 100)) ?><sup>đ</sup> <del><?= number_format($don_gia) ?><sup>đ</sup></del></p>
+                                    <p><?= number_format($don_gia - ($giam_gia * $don_gia / 100)) ?><sup>đ</sup><?php 
+                                        if($giam_gia != 0 && $giam_gia != null){
+                                            echo '<del>'.number_format($don_gia).'<sup>đ</sup></del>';
+                                        }
+                                    ?></p>
                                 </div>
                             </div>
                         </a>
