15abacaf446421d2293e9880be104b4e9daf8f18
dattmpc02133 <datgoodboy2002@gmail.com>
2021-11-27 15:38:29 +0700

    css sytle

---
+++ site/tin-tuc/tin-tuc.php
@@ -0,0 +1,13 @@
+<?php 
+require_once("../../global.php");
+require_once("../../DAO/pdo.php");
+require_once("../../DAO/cart.php");
+require_once("../../DAO/san-pham.php");
+require_once("../../DAO/loai.php");
+extract($_REQUEST);
+
+
+
+$VIEW_NAME = "tin-tuc-ui.php";
+require("../layout.php");
+?>
--- tin-tuc/tin-tuc.php
+++
@@ -1,8 +0,0 @@
-<?php 
-extract($_REQUEST);
-
-
-
-$VIEW_NAME = "tin-tuc-ui.php";
-require("../layout.php");
-?>
